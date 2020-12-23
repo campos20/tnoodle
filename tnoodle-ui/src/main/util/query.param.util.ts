11c68fecd62bb2fc6cf5f1312247148b2608d4b6
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2020-12-23 07:37:03 -0300

    Migrate util file to ts

--- tnoodle-ui/src/main/util/query.param.util.ts
+++ tnoodle-ui/src/main/util/query.param.util.ts
@@ -1,22 +1,28 @@
-export function getHashParameter(name) {
+/**
+ * Get the parameter value after the # sign
+ */
+export function getHashParameter(name: string) {
     return parseQueryString(window.location.hash)[name];
 }
 
-export function getQueryParameter(name) {
+/**
+ * Get parameter value after the ? sign
+ */
+export function getQueryParameter(name: string) {
     let urlSplit = window.location.href.split("?");
     let lastElement = urlSplit.slice(-1)[0];
     return parseQueryString(lastElement)[name];
 }
 
 // Copied from https://stackoverflow.com/a/3855394/1739415
-function parseQueryString(query) {
+function parseQueryString(query: string) {
     if (!query) {
         return {};
     }
 
     return (/^[?#]/.test(query) ? query.slice(1) : query)
         .split("&")
-        .reduce((params, param) => {
+        .reduce((params: any, param) => {
             let [key, value] = param.split("=");
             params[key] = value
                 ? decodeURIComponent(value.replace(/\+/g, " "))
