3899c45bd85d7318a33a7f3c3c6ddb614a657da3
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2020-12-27 17:22:44 -0300

    Move util to ts

--- tnoodle-ui/src/main/util/query.param.util.ts
+++ tnoodle-ui/src/main/util/query.param.util.ts
@@ -1,15 +1,15 @@
-export function getHashParameter(name) {
+export function getHashParameter(name: string) {
     return parseQueryString(window.location.hash)[name];
 }
 
-export function getQueryParameter(name) {
+export function getQueryParameter(name: string) {
     let urlSplit = window.location.href.split("?");
     let lastElement = urlSplit.slice(-1)[0];
     return parseQueryString(lastElement)[name];
 }
 
 // Copied from https://stackoverflow.com/a/3855394/1739415
-function parseQueryString(query) {
+function parseQueryString(query: string) {
     if (!query) {
         return {};
     }
@@ -25,12 +25,13 @@ function parseQueryString(query) {
         }, {});
 }
 
-const setPageWithoutRedirect = (url) => window.history.pushState(null, "", url);
+const setPageWithoutRedirect = (url: string) =>
+    window.history.pushState(null, "", url);
 
 const currentLocationWithoutQuery = () =>
     window.location.origin + window.location.pathname;
 
-export const updateQueryParam = (name, value) => {
+export const updateQueryParam = (name: string, value: string) => {
     var searchParams = new URLSearchParams(window.location.search);
     searchParams.set(name, value);
     setPageWithoutRedirect(
@@ -38,7 +39,7 @@ export const updateQueryParam = (name, value) => {
     );
 };
 
-export const removeQueryParam = (name) => {
+export const removeQueryParam = (name: string) => {
     var searchParams = new URLSearchParams(window.location.search);
     searchParams.delete(name);
     setPageWithoutRedirect(
